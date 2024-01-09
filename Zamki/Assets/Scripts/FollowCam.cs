46469fe2e5885da47c0a94a88a9e7b9fa6253037
novakvova <novakvova@gmail.com>
2024-01-09 20:14:18 +0200

    Begin working cloud

--- Zamki/Assets/Scripts/FollowCam.cs
+++ Zamki/Assets/Scripts/FollowCam.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     public static GameObject POI;
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+    [Header("Set Dynamically")]
+    public float camZ;
+
+    
+
+    private void Awake()
     {
-        
+        camZ = this.transform.position.z;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
     {
-        
+        if (POI == null) return;
+        Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(destination.x, minXY.x);
+        destination.y = Mathf.Max(destination.y, minXY.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
