aa86936eac262d7f7227461fa483d4e38feec363
novakvova <novakvova@gmail.com>
2024-01-13 16:59:38 +0200

    Add Goal is good to end

--- Zamki/Assets/Scripts/FollowCam.cs
+++ Zamki/Assets/Scripts/FollowCam.cs
@@ -33,7 +33,7 @@ public class FollowCam : MonoBehaviour
         else
         {
             destination = POI.transform.position;
-            if(POI.tag == "Porjectile")
+            if(POI.tag == "Projectile")
             {
                 if(POI.GetComponent<Rigidbody>().IsSleeping())
                 {
--- Zamki/Assets/Scripts/Goal.cs
+++ Zamki/Assets/Scripts/Goal.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    static public bool goalMet = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag== "Projectile")
+        {
+            Goal.goalMet = true;    
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
